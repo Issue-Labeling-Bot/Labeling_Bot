a726489b8e34e5535d63a669c756f6bdb8e53eca
Choi-kwang-hyun <jpg04260@naver.com>
2020-11-24 14:56:29 +0900

    파이썬 파일 분리

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 import json,requests
 
-from github3 import login
+
 
 
 @csrf_exempt
@@ -12,20 +12,4 @@ def post_list(request):
 
     jsondata = request.body
     
-    d = json.loads(jsondata)
-    state = d['action']
-    issues = d['issue']
-    token='226e5d255cccfa18c305402f63e5d91d33082c3e'
-    title = issues['title']
-    body = issues['body']
-    label = issues['labels']
-    number = issues['number']
-    print(number)
-    if state == "opened":
-        gh = login('Issue-Labeling-Bot', password='quf**2266')
-        gh = login(token=token)
-        issue = gh.issue('Issue-Labeling-Bot','Labeling_Bot', number)      
-        issue.add_labels('enhancement')
-        issue.add_labels('bug')
-        
     return HttpResponse("pong")
