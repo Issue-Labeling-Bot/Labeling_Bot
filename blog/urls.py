e2658d944aa1fdb15b5e54a9b8ce9e042d0e2983
DESKTOP-R63835P\jpg04 <jpg04260@naver.com>
2020-12-03 17:46:20 +0900

    add Server File

---
+++ blog/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('api/Labeling/', views.post_list, name='Labeling'),
+]
---
+++ blog/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
+
+from executor import Executor
+
+
+
+@csrf_exempt
+def post_list(request):
+
+    jsondata = request.body
+    Executor(jsondata)
+
+        
+    return HttpResponse("pong")
+
+
+
